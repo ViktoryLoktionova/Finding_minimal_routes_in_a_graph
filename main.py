30688028cf3a5485f59da8589625f7b6942a476e
Victor <bulkin_viktor@mail.ru>
2021-08-19 17:20:19 +0300

    using a genetic algorithm looking for the shortest route in the graph.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from deap import base, algorithms
 from deap import creator
 from deap import tools
 
+from graph_show import show_graph
+
 import random
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,37 +16,86 @@ D = ((0, 3, 1, 3, inf, inf),
      (inf, inf, 7, inf, 0, 4),
      (inf, inf, 5, 2, 4, 0))
 
-startV = 0 #первая вершина
+startV = 0              # стартовая вершина
 LENGTH_D = len(D)
-LENGTH_CHROM = len(D)*len(D[0]) #длина хромосомы, подлежащей оптимизации
+LENGTH_CHROM = len(D)*len(D[0])    # длина хромосомы, подлежащей оптимизации
 
-POPULATION_SIZE = 500 # размер популяции
-P_CROSSOVER = 0.9 # вероятность скрещивания
-P_MUTATION = 0.1 # вероятность мутации
-MAX_GENERATIONS = 30 # максимальное количество поколений
+# константы генетического алгоритма
+POPULATION_SIZE = 500   # количество индивидуумов в популяции
+P_CROSSOVER = 0.9       # вероятность скрещивания
+P_MUTATION = 0.1        # вероятность мутации индивидуума
+MAX_GENERATIONS = 30    # максимальное количество поколений
+HALL_OF_FAME_SIZE = 1
+
+hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
 
 RANDOM_SEED = 42
 random.seed(RANDOM_SEED)
 
-creator.create("FitnessMin", base.Fitness, weights = (-1.0))
-creator.create("Individual", list, fitness = creator.FitnessMin)
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMin)
 
 toolbox = base.Toolbox()
-toolbox.register("randomOrder", random.sample, range(LENGTH_D), LENGTH_D) #на выходе дает список случайных значений
+toolbox.register("randomOrder", random.sample, range(LENGTH_D), LENGTH_D)
 toolbox.register("individualCreator", tools.initRepeat, creator.Individual, toolbox.randomOrder, LENGTH_D)
-toolbox.register("poopulationCreator", tools.initRepeat, list, toolbox.individualCreator)
-
+toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
 
 population = toolbox.populationCreator(n=POPULATION_SIZE)
 
+
 def dikstryFitness(individual):
-     s = 0
-     for n, path in enumerate(individual):
-          path = path[:path.index(n)+1]
+    s = 0
+    for n, path in enumerate(individual):
+        path = path[:path.index(n)+1]
 
-          si = startV
-          for j in path:
-               s += D[si], [j]
+        si = startV
+        for j in path:
+            s += D[si][j]
+            si = j
 
-     return s,
+    return s,         # кортеж
 
+def cxOrdered(ind1, ind2):
+    for p1, p2 in zip(ind1, ind2):
+        tools.cxOrdered(p1, p2)
+
+    return ind1, ind2
+
+def mutShuffleIndexes(individual, indpb):
+    for ind in individual:
+        tools.mutShuffleIndexes(ind, indpb)
+
+    return individual,
+
+
+toolbox.register("evaluate", dikstryFitness)
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("mate", cxOrdered)
+toolbox.register("mutate", mutShuffleIndexes, indpb=1.0/LENGTH_CHROM/10)
+
+stats = tools.Statistics(lambda ind: ind.fitness.values)
+stats.register("min", np.min)
+stats.register("avg", np.mean)
+
+population, logbook = algorithms.eaSimple(population, toolbox,
+                                        cxpb=P_CROSSOVER/LENGTH_D,
+                                        mutpb=P_MUTATION/LENGTH_D,
+                                        ngen=MAX_GENERATIONS,
+                                        halloffame=hof,
+                                        stats=stats,
+                                        verbose=True)
+
+maxFitnessValues, meanFitnessValues = logbook.select("min", "avg")
+
+best = hof.items[0]
+print(best)
+
+plt.plot(maxFitnessValues, color='red')
+plt.plot(meanFitnessValues, color='green')
+plt.xlabel('Поколение')
+plt.ylabel('Макс/средняя приспособленность')
+plt.title('Зависимость максимальной и средней приспособленности от поколения')
+
+fig, ax = plt.subplots()
+show_graph(ax, best)
+plt.show()
